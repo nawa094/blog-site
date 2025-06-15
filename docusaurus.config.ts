39cc8fb16a1f0e80faca0553733cd1f629eeb004
Nawa Mumbwe <mumbwe94@gmail.com>
2025-06-15 18:29:17 +0200

    Set up github pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+// Replace these values:
+const organizationName = "nawa094"; // Or org name
+const projectName = "blog-site"; // Repo name
 
 const config: Config = {
   title: "Nawa Mumbwe",
@@ -15,15 +18,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: `https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName, // Usually your GitHub org/user name.
+  projectName, // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
