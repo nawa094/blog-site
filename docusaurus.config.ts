d35649a1315bd227cec472ed73b328eb25ea4c9b
Nawa Mumbwe <mumbwe94@gmail.com>
2025-06-21 21:33:43 +0200

    add new social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -71,7 +71,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/social_card_preview.png",
 
     navbar: {
       title: "NM",
